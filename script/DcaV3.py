7826efadc3fd16535cfbc0122a56ea6774938395
arsyadCode <muhammadarsyadv2@gmail.com>
2024-08-12 10:02:35 +0700

    Add V4 and fix V3

--- script/DcaV3.py
+++ script/DcaV3.py
@@ -8,7 +8,6 @@ import curve_cum_function as ccf
 from dash import dcc, html
 from dash.dependencies import Input, Output
 from datetime import datetime
-from scipy.interpolate import interp1d
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = 'DCA'
@@ -24,6 +23,14 @@ app.layout = dbc.Container([
             dcc.Dropdown(id='well-name', options=[{'label': name, 'value': name} for name in sheet_names], value=sheet_names[0])
         ], width=4),
         dbc.Col([
+            html.Label('Start Forecast Date:'),
+            dcc.DatePickerSingle(
+                id='foil-date-picker',
+                date=datetime(2024, 4, 1),
+                display_format='DD/MM/YYYY'
+            )
+        ], width=2),
+        dbc.Col([
             html.Label('Rate Intervention:'),
             dcc.Input(id='oil-rate-intervention', type='number', value=0, step=100)
         ], width=2),
@@ -38,22 +45,9 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             html.Label('Select Initial Date:'),
-            dcc.Slider(
-                id='initial-date-slider', 
-                min=0, 
-                step=1, 
-                value=0, 
-                marks={}, 
-                tooltip={"placement": "bottom", "always_visible": True}
-            )
-        ], width=12),
-    ]),
-    dbc.Row([
-        dbc.Col([
-            html.Label('Select End Date:'),
-            dcc.Slider(
-                id='end-date-slider', 
-                min=0, 
+            dcc.RangeSlider(
+                id='date-slider', 
+                min=0,
                 step=1, 
                 value=0, 
                 marks={}, 
@@ -64,34 +58,34 @@ app.layout = dbc.Container([
 ])
 
 @app.callback(
-    Output('initial-date-slider', 'max'),
-    Output('initial-date-slider', 'marks'),
-    Output('end-date-slider', 'max'),
-    Output('end-date-slider', 'marks'),
+    Output('date-slider', 'max'),
+    Output('date-slider', 'marks'),
     Input('well-name', 'value')
 )
-def update_sliders(well_name):
+def update_slider(well_name):
     df = pd.read_excel(excel_file, sheet_name=well_name)
     df['DATE_STAMP'] = pd.to_datetime(df['DATE_STAMP'], format='%d/%m/%Y', dayfirst=True)
     max_date = df['DATE_STAMP'].max()
     df_filtered = df[df['DATE_STAMP'] <= max_date]
     marks = {i: {'label': '', 'style': {'display': 'none'}} for i, date in enumerate(df_filtered['DATE_STAMP'])}
-    return len(df_filtered) - 1, marks, len(df_filtered) - 1, marks
+    return len(df_filtered) - 1, marks
 
 @app.callback(
     Output('oil-plot', 'figure'),
     Input('well-name', 'value'), 
-    Input('initial-date-slider', 'value'),
-    Input('end-date-slider', 'value'),
+    Input('foil-date-picker', 'date'),
     Input('months-end-date', 'value'),
+    Input('date-slider', 'value'),
     Input('oil-rate-intervention', 'value')
 )
-def update_plots(well_name, initial_slider_value, end_slider_value, months_end_date, rate_intervention):
+def update_plots(well_name, foil_date, months_end_date, slider_value, rate_intervention):
+    foil_date = pd.to_datetime(foil_date)
+    
     df = pd.read_excel(excel_file, sheet_name=well_name)
     df['DATE_STAMP'] = pd.to_datetime(df['DATE_STAMP'], format='%d/%m/%Y', dayfirst=True)
     
-    start_date = df['DATE_STAMP'].iloc[initial_slider_value]
-    end_date = df['DATE_STAMP'].iloc[end_slider_value]
+    start_date = df['DATE_STAMP'].iloc[slider_value]
+    end_date = df['DATE_STAMP'].max()
     
     df_filtered = df[(df['DATE_STAMP'] >= start_date) & (df['DATE_STAMP'] <= end_date)]
     
@@ -99,7 +93,12 @@ def update_plots(well_name, initial_slider_value, end_slider_value, months_end_d
     
     last_rate = df[df['CORR_OIL_RATE_STBD'] != 0]['CORR_OIL_RATE_STBD'].iloc[-1] + rate_intervention # Need to be validation
 
-    forecast_df = pd.DataFrame({'DATE': df_oil['DATE_STAMP'], 'TIME': range(len(df_oil['DATE_STAMP']))})
+    foil_date = foil_date.replace(day=1)
+    end_date = foil_date + pd.DateOffset(months=months_end_date)
+    date_range = pd.date_range(start=foil_date, end=end_date, freq='MS')
+    marker_y = df[df['DATE_STAMP'] == start_date]['CORR_OIL_RATE_STBD'].values[0]
+
+    forecast_df = pd.DataFrame({'DATE': date_range, 'TIME': range(len(date_range))})
     forecast_df['Forecast_Oil_Exponential'] = ccf.exponential_rate(last_rate, oil_exp_di, forecast_df['TIME'])
     forecast_df['Forecast_Oil_Hyperbolic'] = ccf.hyperbolic_rate(last_rate, oil_hyper_di, best_b_oil, forecast_df['TIME'])
     forecast_df['Forecast_Oil_Harmonic'] = ccf.harmonic_rate(last_rate, oil_har_di, forecast_df['TIME'])
@@ -112,8 +111,7 @@ def update_plots(well_name, initial_slider_value, end_slider_value, months_end_d
     oil_fig.add_trace(go.Scatter(x=forecast_df['DATE'], y=forecast_df['Forecast_Oil_Exponential'], mode='lines', name='Exponential Forecast', line=dict(color='LightBlue')))
     oil_fig.add_trace(go.Scatter(x=forecast_df['DATE'], y=forecast_df['Forecast_Oil_Hyperbolic'], mode='lines', name='Hyperbolic Forecast', line=dict(color='LightGreen')))
     oil_fig.add_trace(go.Scatter(x=forecast_df['DATE'], y=forecast_df['Forecast_Oil_Harmonic'], mode='lines', name='Harmonic Forecast', line=dict(color='LightSalmon')))
-    oil_fig.add_trace(go.Scatter(x=[start_date], y=[df.loc[initial_slider_value, 'CORR_OIL_RATE_STBD']], mode='markers', name='Selected Start Date', marker=dict(color='black', size=8)))
-    oil_fig.add_trace(go.Scatter(x=[end_date], y=[df.loc[end_slider_value, 'CORR_OIL_RATE_STBD']], mode='markers', name='Selected End Date', marker=dict(color='black', size=8)))
+    oil_fig.add_trace(go.Scatter(x=[start_date], y=[marker_y], mode='markers', name='Selected Date', marker=dict(color='black', size=8)))
     oil_fig.update_layout(title=f'Oil Rate Comparison of Decline Models Well {well_name}', xaxis_title='Date', yaxis_title='Oil Rate (bopd)', xaxis=dict(tickformat='%Y-%m-%d'))
 
     return oil_fig
