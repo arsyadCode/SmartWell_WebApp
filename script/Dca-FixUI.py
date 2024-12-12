f94c64ca80b5e6c11e3e1d70c73abf9719f38c86
arsyadCode <muhammadarsyadv2@gmail.com>
2024-12-12 15:10:00 +0700

    dev_tools turn to False

--- script/Dca-FixUI.py
+++ script/Dca-FixUI.py
@@ -668,4 +668,4 @@ def download_table(n_clicks, rows):
 if __name__ == '__main__':
     if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
         webbrowser.open_new("http://127.0.0.1:8050/")
-    app.run_server(debug=True, dev_tools_ui=True)
+    app.run_server(debug=True, dev_tools_ui=False)
